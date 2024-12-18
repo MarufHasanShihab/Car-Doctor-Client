15adcc55e16018156ded8beed438ff41710c6e12
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-18 16:57:22 +0600

    change services data load api link

--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -4,7 +4,7 @@ import ServiceCard from "./ServiceCard";
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((response) => response.json()) // This expects JSON
       .then((data) => setServices(data))
       .catch((error) => console.error(error));
@@ -22,11 +22,15 @@ const Services = () => {
           believable.
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-6">
+      {
+        services.length > 0 ? <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-6">
         {
             services?.map(service => <ServiceCard key={service._id} service={service}/>)
         }
+      </div> : <div className="min-h-[40vh] flex items-center justify-center">
+        <h2 className="text-[#151515] text-[20px] text-center md:text-[25px] capitalize font-bold border border-gray-400 p-5 rounded-lg">No service is currently available.</h2>
       </div>
+      }
       <div className="flex justify-center my-12">
       <button className="py-2 px-4 text-[#ff3811] border border-[#ff3811]">More Services</button>
       </div>
